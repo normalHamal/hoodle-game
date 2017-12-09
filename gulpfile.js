afcdce26940df2fdc8632e001ca2ccd600edfac3
normalHamal <3292106931@qq.com>
2017-12-09 19:33:17 +0800

    添加游戏出来的过渡效果

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp   = require('gulp');
-var	concat = require('gulp-concat');   //合并文件
-var uglify = require('gulp-uglify');   //js压缩
-var rename = require('gulp-rename');   //文件重命名
-var notify = require('gulp-notify');   //提示
+var	concat = require('gulp-concat');    //合并文件
+var uglify = require('gulp-uglify');    //js压缩
+var rename = require('gulp-rename');    //文件重命名
+var notify = require('gulp-notify');    //提示
+var server = require('gulp-webserver'); //本地服务
 
 gulp.task('minJs', function () {
 	return gulp.src([
@@ -23,4 +24,17 @@ gulp.task('minJs', function () {
        .pipe(uglify())                    //压缩
        .pipe(gulp.dest('dist/js'))            //输出 
        .pipe(notify({message:"minJs task ok"}));    //提示
+});
+
+gulp.task('server', function () {
+	gulp.src('./')
+		.pipe(server({
+			port: 80,//端口
+			host: '127.0.0.1',//域名
+			liveload: true,//实时刷新代码。不用f5刷新
+			directoryListing: {
+				path: './',
+				enable: true
+			}
+		}))
 });
